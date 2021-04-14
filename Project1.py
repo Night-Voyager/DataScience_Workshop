364c935ddf705ccd820ce6f85417c22fcaf7b56a
Night-Voyager <18722007@bjtu.edu.cn>
2021-04-15 00:00:13 +0800

    Finish project 1

--- Project1.py
+++ Project1.py
@@ -2,13 +2,20 @@ import re
 import requests
 import json
 
+import pandas as pd
+
 request = requests.get(url="https://www.timeanddate.com/weather/uk/london/historic")
 init_data_js = re.findall(re.compile(r"var data=.*?}}};"), request.text)[0]
 init_data_json = json.loads(init_data_js.lstrip("var data=").rstrip(";"))
 
-weather_data_json = init_data_json['detail']
+weather_json = init_data_json['detail']
+weather_dataframe = pd.DataFrame.from_dict(weather_json)
+print(weather_dataframe)
 
-print(weather_data_json)
+# weather_dataframe.to_csv("weather.csv", encoding="utf_8_sig")
+
+# with open("init_data.json", 'w') as file:
+#     json.dump(init_data_json, file)
 
 # file = open("request_file.html", 'w', encoding="utf-8")
 # file.write(request.text)

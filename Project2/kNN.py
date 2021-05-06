fdf78380bf1674ed3bfa497a5732f1a89df3bacc
Night-Voyager <18722007@bjtu.edu.cn>
2021-05-06 08:22:36 +0800

    Add Project2

---
+++ Project2/kNN.py
@@ -0,0 +1,68 @@
+from numpy import *
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    """k-近邻算法"""
+
+    # 距离计算
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+
+    # 选择距离最小的k个点
+    sotedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sotedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    # 排序
+    sortedClassCount = sorted(classCount.items(),
+                              key=operator.itemgetter(1),
+                              reverse=True)
+
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    """将文本记录转换为NumPy的解析程序"""
+
+    # 得到文件行数
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+
+    # 创建返回的NumPy矩阵
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+
+    # 解析文件数据到列表
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet/tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
